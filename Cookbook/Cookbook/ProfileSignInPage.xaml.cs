5a2d9eff1183a4dc499063043fc0e7321a4fd64d
CS\cynguyen <cynthia_nguyen25@hotmail.com>
2018-11-22 09:14:34 -0700

    added profile pages

---
+++ Cookbook/Cookbook/ProfileSignInPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cookbook
+{
+    /// <summary>
+    /// Interaction logic for ProfileSignInPage.xaml
+    /// </summary>
+    public partial class ProfileSignInPage : Page
+    {
+        private ProfileInfoPage profileInfoPage = new ProfileInfoPage();
+
+        public ProfileSignInPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SignIn_Click(object sender, RoutedEventArgs e)
+        {
+            signInMain.Content = profileInfoPage;
+        }
+    }
+}
---
+++ Cookbook/Cookbook/ProfileSignUpPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cookbook
+{
+    /// <summary>
+    /// Interaction logic for ProfileSignUpPage.xaml
+    /// </summary>
+    public partial class ProfileSignUpPage : Page
+    {
+        private ProfileInfoPage profileInfoPage = new ProfileInfoPage();
+
+        public ProfileSignUpPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            signUpMain.Content = profileInfoPage;
+        }
+    }
+}
