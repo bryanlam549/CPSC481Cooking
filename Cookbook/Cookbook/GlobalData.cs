2958fa4489a6b54ebdb548d0195dc32cac97ba98
Aaron Hornby (Laptop) <aaron.hornby@outlook.com>
2018-11-23 11:30:58 -0700

    + GlobalData class to store reference to data needed in different parts of file (preserve state everywhere)

---
+++ Cookbook/Cookbook/GlobalData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.Windows.Controls;
+
+
+namespace Cookbook
+{
+    public class GlobalData
+    {
+        private static GlobalData instance = new GlobalData();
+
+        public static GlobalData Instance
+        {
+            get { return instance; }
+        }
+
+        public Recipe recipe1 = new Recipe(); // something like this, then each component like a HeartButton could have a field set to 1 recipe.
+        public Recipe recipe2 = new Recipe();
+
+        public Page page1 = new Page();
+
+        
+
+        private GlobalData()
+        {
+            // init fields of each recipe here
+            // init fields of each page here
+
+            
+
+            Page page = GlobalData.Instance.page1;
+        }
+
+
+
+        
+
+
+
+
+
+
+
+    }
+}
