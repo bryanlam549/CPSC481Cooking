106e8dbdf6de022c8fce108e4ac3418e765f00f2
CS\cynguyen <cynthia_nguyen25@hotmail.com>
2018-11-23 11:59:18 -0700

    added stuff for profile sign in

--- Cookbook/Cookbook/ProfileSignInPage.xaml.cs
+++ Cookbook/Cookbook/ProfileSignInPage.xaml.cs
@@ -21,16 +21,38 @@ namespace Cookbook
     public partial class ProfileSignInPage : Page
     {
         //private ProfileMainPage profileMainPage = new ProfileMainPage("", "", "");
+        private List<string> accountList = new List<string>();
 
         public ProfileSignInPage()
         {
             InitializeComponent();
+
+            accountList.Add("foodluver123");
+            accountList.Add("ilovefood456@food.com");
+            accountList.Add("123456789123456789123456789");
+
+            accountList.Add("foodisLIFE");
+            accountList.Add("burgers@food.com");
+            accountList.Add("123456789");
         }
+
 
         private void SignIn_Click(object sender, RoutedEventArgs e)
         {
-            ProfileMainPage profileMainPage = new ProfileMainPage(UserInput.Text, "", PasswordInput.Password);
-            signInMain.Content = profileMainPage;
+
+
+            if (accountList.Contains(UserInput.Text) && accountList.Contains(PasswordInput.Password))
+            {
+
+                ProfileMainPage profileMainPage = new ProfileMainPage(UserInput.Text, accountList[(accountList.IndexOf(UserInput.Text)) + 1], PasswordInput.Password);
+                signInMain.Content = profileMainPage;
+            }
+            else
+            {
+                invalidInput.Text = "Invalid username or password";
+               
+            }
+            
         }
     }
 }
