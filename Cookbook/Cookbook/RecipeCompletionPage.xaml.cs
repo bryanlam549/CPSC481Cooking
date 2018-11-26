35b58de34a0d7c34f87e27c6f67a57fc62dfbb92
CS\cynguyen <cynthia_nguyen25@hotmail.com>
2018-11-26 09:47:36 -0700

    added outline for recipe completion page

---
+++ Cookbook/Cookbook/RecipeCompletionPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cookbook
+{
+    /// <summary>
+    /// Interaction logic for RecipeCompletionPage.xaml
+    /// </summary>
+    public partial class RecipeCompletionPage : Page
+    {
+        public RecipeCompletionPage()
+        {
+            InitializeComponent();
+        }
+
+        private void recipeCompleteMain_Navigated(object sender, NavigationEventArgs e)
+        {
+
+        }
+    }
+}
